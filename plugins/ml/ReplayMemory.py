30c4232cc426fe5885f9e345f6a2287b9b90399a
Unknown <bart.lp@hotmail.com>
2018-08-24 14:25:51 +0200

    Add replay memory class

---
+++ plugins/ml/ReplayMemory.py
@@ -0,0 +1,37 @@
+from collections import deque
+import random
+
+class ReplayMemory():
+
+    def __init__(self, memory_size):
+        self.memory_size = memory_size
+        self.num_experiences = 0
+        self.memory = deque()
+
+    def getBatch(self, batch_size):
+        # Randomly sample batch_size examples
+        if self.num_experiences < batch_size:
+            return random.sample(self.memory, self.num_experiences)
+        else:
+            return random.sample(self.memory, batch_size)
+
+    def size(self):
+        return self.memroy_size
+
+    def add(self, state, action, reward, new_state, done):
+        experience = (state, action, reward, new_state, done)
+        if self.num_experiences < self.memory_size:
+            self.memory.append(experience)
+            self.num_experiences += 1
+        else:
+            self.memory.popleft()
+            self.memroy.append(experience)
+
+    def count(self):
+        # if buffer is full, return buffer size
+        # otherwise, return experience counter
+        return self.num_experiences
+
+    def erase(self):
+        self.memory = deque()
+        self.num_experiences = 0
