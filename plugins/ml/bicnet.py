93749a10adad02eb5ac4b4418008ade2254d1035
Unknown <bart.lp@hotmail.com>
2018-10-01 21:37:29 +0200

    Running simple simulation

--- plugins/ml/bicnet.py
+++ plugins/ml/bicnet.py
@@ -1,19 +1,26 @@
 from keras.models import Model
-from keras.layers import Dense, Input, TimeDistributed, Bidirectional, LSTM, Concatenate, Masking
+from keras.layers import Dense, Input, TimeDistributed, Bidirectional, LSTM, Concatenate, Masking, Activation, Multiply
 from keras.optimizers import Adam
+import keras
+import tensorflow as tf
 
 class BiCNet:
     @staticmethod
     def build_actor(max_agents, obs_dim, act_dim, H1, H2, name):
         S = Input(shape=(max_agents, obs_dim), name=name+'_input_states')
+        bool_mask = Input(shape =(max_agents, act_dim), dtype=tf.float32, name=name+'_bool_mask')
         # Set the masking value to ignore 0 padded sequence inputs.
         mask = Masking(mask_value=0.)(S)
         h0 = TimeDistributed(Dense(H1, activation='relu'))(mask)
         h1 = Bidirectional(LSTM(H2, return_sequences=True))(h0)
-        h2 = TimeDistributed(Dense(act_dim, activation='linear', name=name+'_output'))(h1)
-        out = h2
-        model = Model(inputs=S, outputs=out)
-        return model, model.trainable_weights, out, S
+        h2 = TimeDistributed(Dense(act_dim, activation='linear', name=name + '_output'))(h1)
+        out1 = TimeDistributed(Dense(act_dim, activation='linear', name=name+'_actionoutput'))(h1)
+        out2 = TimeDistributed(Dense(act_dim, activation='linear', name=name+'_speedoutput'))(h1)
+        h3 = Multiply()([h2, bool_mask])
+        actions = Activation('softmax', name=name+'_softmax')(h3)
+        out = actions
+        model = Model(inputs=[S, bool_mask], outputs=out)
+        return model, model.trainable_weights, out, S, bool_mask
 
     @staticmethod
     def build_critic(max_agents, obs_dim, act_dim, H1, H2, LR, name):
--- plugins/thesis.py
+++ plugins/thesis.py
@@ -37,9 +37,10 @@ import tensorflow as tf
 ### Initialization function of your plugin. Do not change the name of this
 ### function, as it is the way BlueSky recognises this file as a plugin.
 def init_plugin():
-    global env, agent
+    global env, agent, update_interval
     env = Environment()
     agent = Agent()
+    update_interval = 10
     config = {
         # The name of your plugin
         'plugin_name':     'THESIS',
@@ -50,7 +51,7 @@ def init_plugin():
         # Update interval in seconds. By default, your plugin's update function(s)
         # are called every timestep of the simulation. If your plugin needs less
         # frequent updates provide an update interval.
-        'update_interval': 1,
+        'update_interval': update_interval,
 
         # The update function is called after traffic is updated. Use this if you
         # want to do things as a result of what happens in traffic. If you need to
@@ -94,15 +95,7 @@ def init_plugin():
 #            'Reset the training environment']
             }
 
-    stack.stack("CRE BART001, B737, 51.862982, 2.830079, 90, fl200, 150")
-    stack.stack("BART001 DEST EHAM RWY06")
-    stack.stack("BART001 VNAV ON")
-    stack.stack("CRE BART002, B737, 51.2, 2.83, 90, FL200, 150")
-    stack.stack("BART002 DEST EHAM RWY06")
-    stack.stack("BART002 VNAV ON")
-    stack.stack("CRE BART003, B737, 50.9, 2.83, 90, FL200, 150")
-    stack.stack("BART003 DEST EHAM RWY06")
-    stack.stack("BART003 VNAV ON")
+    env.reset()
 
     # init_plugin() should always return these two dicts.
     return config, stackfunctions
@@ -119,36 +112,41 @@ def update():
 
 
 def init():
-    load_routes()
-    stack.stack("BART001 DEST EHAM RWY06")
+    # load_routes()
+    pass
 
 
 def preupdate():
     # Initialize routes
-    if sim.simt < 1.5:
-        init()
+    if sim.simt < update_interval*1.5:
+        # init()
         new_state = env.init()
-        stack.stack("CRE BART004, B737, 51.862982, 2.830079, 90, fl200, 150")
-        stack.stack("BART004 DEST EHAM RWY06")
-        stack.stack("BART004 VNAV ON")
+        # stack.stack("CRE BART004, B737, 51.862982, 2.830079, 90, fl200, 150")
+        # stack.stack("BART004 DEST EHAM RWY06")
+        # stack.stack("BART004 VNAV ON")
 
     else:
         # Construct new state
         state, reward, new_state, done = env.step()
         # print(agent.replay_memory.count(), state)
+        print('reward')
+        agent.update_cumreward(reward)
         agent.update_replay_memory(state, reward, done, new_state)
         if agent.replay_memory.num_experiences > agent.batch_size:
             agent.train()
-        agent.write_summaries(reward)
+        # agent.write_summaries(reward)
 
     agent.act(new_state)
+    if env.done:
+        # Reset environment states and agent states
+        env.reset()
+        agent.reset()
 
 
     pass
 
 
 def reset():
-
     pass
 
 
@@ -188,14 +186,14 @@ class Environment:
             self.ac_dict[id]= [0,0,0]
 
     def init(self):
-        self.generate_observation()
+        self.observation = self.generate_observation()
         return self.observation
 
     def step(self):
         prev_observation = self.observation
         self.observation = self.generate_observation()
         # Check termination conditions
-        self.check_termination()
+        self.check_reached()
         self.generate_reward()
         return prev_observation, self.reward, self.observation, self.done
 
@@ -203,6 +201,11 @@ class Environment:
         global_reward = 1
         local_reward = np.ones((traf.ntraf,1))
         self.reward = local_reward + global_reward
+        if self.done:
+            self.reward = 100 * np.ones((traf.ntraf,1))
+        else:
+            self.reward = -1 * np.ones((traf.ntraf,1))
+
 
     def generate_observation(self):
         # Produce a running average off all variables in the field with regard to the initial state convergence that is being tackled in the problem.
@@ -211,13 +214,55 @@ class Environment:
         obs = self.state_normalizer.normalize(obs)
         return obs
 
-    def check_termination(self):
-        self.done = False
+        # def reached(self, qdr, dist, flyby):
+        #     # Calculate distance before waypoint where to start the turn
+        #     # Turn radius:      R = V2 tan phi / g
+        #     # Distance to turn: wpturn = R * tan (1/2 delhdg) but max 4 times radius
+        #     # using default bank angle per flight phase
+        #
+        #
+        #
+        #     # Avoid circling by checking for flying away
+        #     away = np.abs(degto180(bs.traf.trk % 360. - qdr % 360.)) > 90.  # difference large than 90
+        #
+        #     # Ratio between distance close enough to switch to next wp when flying away
+        #     # When within pro1 nm and flying away: switch also
+        #     proxfact = 1.01  # Turnradius scales this contant , factor => [turnrad]
+        #     incircle = dist < turnrad * proxfact
+        #     circling = away * incircle  # [True/False] passed wp,used for flyover as well
+        #     nonflyby = (flyby == 1) * away * (np.array(dist) < 1000)
+        #     #        print("NONFLYBY ", nonflyby)
+        #     # Check whether shift based dist is required, set closer than WP turn distance
+        #     swreached = np.where(bs.traf.swlnav * ((dist < self.turndist) + circling + nonflyby))[0]
+        #
+        #     # Return True/1.0 for a/c where we have reached waypoint
+        #     return swreached
+
+    def check_reached(self):
+        qdr, _ = qdrdist(traf.lat, traf.lon,
+                                traf.actwp.lat, traf.actwp.lon)  # [deg][nm])
+
+        # check which aircraft have reached their destination by checkwaypoint type = 3 (destination) and the relative
+        # heading to this waypoint is exceeding 150
+        dest = np.asarray([traf.ap.route[i].wptype[traf.ap.route[i].iactwp] for i in range(traf.ntraf)]) == 3
+        away = np.abs(degto180(traf.trk % 360. - qdr % 360.)) > 150.
+        reached = np.where(away * dest)[0]
+        # reached = self.Reached(qdr, dist, traf.actwp.flyby)
+        # if reached!=[]:
+        #     print(reached)
+        if reached==[0]:
+            # TODO: Extend for multi-aircraft
+            self.done = True
+        else:
+            self.done = False
 
     def generate_commands(self):
         pass
 
-
+    def reset(self):
+        self.done = False
+        stack.stack('open ./scenario/bart/BiCNet.SCN')
+        load_routes()
 
 
 
@@ -225,32 +270,34 @@ class Agent:
     def __init__(self):
         self.ac_dict = {}
         self.speed_values = [175, 200, 225, 250]
-        self.wp_action_size=3
-        self.spd_action_size=len(self.speed_values)
-        self.state_size=5
+        self.wp_action_size = 3
+        self.spd_action_size = len(self.speed_values)
+        self.state_size = 5
         #TODO: Set the correct action size to correspond with the desired action output and neural network architecture
-        self.action_size=1 #self.wp_action_size + self.spd_action_size
-        self.max_agents = 80
+        self.action_size = self.wp_action_size * self.spd_action_size
+        self.max_agents = None
         self.action = []
         self.memory_size = 10000
         self.i = 0
 
 
 
-        self.batch_size = 4
+        self.batch_size = 32
         self.tau = 0.9
         self.gamma = 0.99
         self.critic_learning_rate = 0.001
         self.actor_learning_rate = 0.0001
         self.loss = 0
+        self.cumreward=0
         self.train_indicator = True
 
 
 
-        # config = tf.ConfigProto()
-        # config.gpu_options.allow_growth = True
-        # self.sess = tf.Session(config=config)
-        self.sess = tf.Session()
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        config.allow_soft_placement = True
+        self.sess = tf.Session(config=config)
+        # self.sess = tf.Session()
         K.set_session(self.sess)
 
         self.actor = ActorNetwork(self.sess, self.state_size, self.action_size, self.batch_size, self.tau, self.actor_learning_rate)
@@ -278,7 +325,7 @@ class Agent:
         if array.shape[0] == max_timesteps:
             return array
         else:
-            result = np.zeros((max_timesteps, array.shape[1]))
+            result = np.zeros((max_timesteps, array.shape[-1], self.action_size))
             result[:array.shape[0], :] = array
             return result
 
@@ -311,8 +358,9 @@ class Agent:
     def train(self):
         batch = self.replay_memory.getBatch(self.batch_size)
         for seq in batch:
-            print(seq)
-            print(seq[0].shape)
+            pass
+            # print(seq)
+            # print(seq[0].shape)
 
         # In order to create sequences with equal length for batch processing sequences are padded with zeros to the
         # maximum sequence length in the batch. Keras can handle the zero padded sequences by ignoring the zero
@@ -325,13 +373,14 @@ class Agent:
         rewards = np.asarray([self.pad_zeros(seq[2], max_t) for seq in batch])
         new_states = np.asarray([self.pad_zeros(seq[3], max_t) for seq in batch])
         dones = np.asarray([seq[4] for seq in batch])
-        y_t = actions.copy()
+        mask = np.asarray([self.pad_zeros(seq[5], max_t) for seq in batch])
+        y_t = rewards.copy()
 
         # print(states.shape, states)
         # print('batch', batch)
         # print('new_states - ', new_states.shape)
         # print('predicted', self.actor.target_model.predict(new_states))
-        target_q_values = self.critic.target_model.predict([new_states, self.actor.target_model.predict(new_states)])
+        target_q_values = self.critic.target_model.predict([new_states, self.actor.target_model.predict([new_states, mask])])
         # print(target_q_values.shape)
 
         #Compute the target values
@@ -342,20 +391,23 @@ class Agent:
                 # print(rewards[k].shape, target_q_values[k].shape, y_t[k].shape)
                 y_t[k] = rewards[k] + self.gamma * target_q_values[k]
 
+        print('y_t', y_t.shape)
         if self.train_indicator:
-            print(states.shape, actions.shape, y_t.shape)
+            # print(states.shape, actions.shape, y_t.shape)
             # self.loss += self.critic.model.train_on_batch([states, actions], y_t)
-            actions_for_grad = self.actor.model.predict(states)
+            actions_for_grad = self.actor.model.predict([states, mask])
             grads = self.critic.gradients(states, actions_for_grad)
             self.critic.train(states, actions, y_t)
-            self.actor.train(states, grads)
+            self.actor.train(states, mask, grads)
             self.actor.update_target_network()
             self.critic.update_target_network()
-            print("training epoch succesful")
+            # print("training epoch succesful")
 
 
     def update_replay_memory(self, state, reward, done, new_state):
-        self.replay_memory.add(state, self.action, reward, new_state, done)
+        max_t = state.shape[0]
+        print('update_replay', state.shape, reward.shape, self.action.reshape(max_t, self.action_size).shape, self.mask.reshape(max_t, self.action_size).shape)
+        self.replay_memory.add(state, self.action.reshape(max_t, self.action_size), reward, new_state, done, self.mask.reshape(max_t, self.action_size))
 
     def __update_aircraft_waypoints(self):
         """
@@ -404,14 +456,17 @@ class Agent:
         Creates a mask for valid actions an aircraft can select. Masks the aircraft in the dictionary set that require a new waypoint to be selected with 1.
         :return:
         """
-        actionmask = np.zeros((self.wp_action_size, traf.ntraf))
+        actionmask = np.zeros((traf.ntraf, self.spd_action_size, self.wp_action_size))
         for i in np.arange(traf.ntraf):
             value = self.ac_dict.get(traf.id[i])
+            # If an aircraft does not need a new action [value[0] = 0] so the current action is maintained
             if value[0]==0:
-                actionmask[value[1][2], i]=1
+                print('value', value[1][2])
+                actionmask[i, value[1][2], :]=1
             else:
-                actionmask[:,i]=1
-        return actionmask
+                actionmask[i,:,:]=1
+
+        return actionmask.reshape(1, traf.ntraf, self.action_size)
 
 
     def act(self, state):
@@ -419,23 +474,50 @@ class Agent:
         self.__update_acdict_entries()
         # print(self.ac_dict)
         # __update_aircraft_waypoints()
-
+        print(self.ac_dict)
         # Infer action selection
-        self.action = np.random.random((traf.ntraf, 1))
+        # self.action = np.random.random((traf.ntraf, 1))
+        # actions are grouped per as follows
+
+        #    Waypoints
+        # s [[0, 1, 2, 3],
+        # p [4, 5, 6, 7]
+        # d [8, 9, 10, 11],
+        #   [12, 13, 14, 15]]
+
+
+
         wp_action = np.random.random((self.wp_action_size,traf.ntraf))
         spd_action = np.random.random((self.spd_action_size,traf.ntraf))
 
         # Retrieve mask for action selection
-        mask = self.get_mask()
+        self.mask = self.get_mask()
+
+        self.action = self.actor.predict([np.reshape(state, (1, traf.ntraf, self.state_size)), self.mask])
+        print('action_shape', self.action.shape, self.action)
+        print('mask', self.mask)
+        print(self.action[0,0,:])
+        action = np.zeros((traf.ntraf, 1))
+        for i in range(traf.ntraf):
+            action[i] = np.random.choice(a=np.arange(self.action_size), p=self.action[0,i,:])
+
         # print(mask)
 
         # Select action
-        wp_action_masked = wp_action * mask
+        # print(wp_action_masked)
         # print(wp_action_masked)
 
-        wp_ind = np.argmax(wp_action, axis=0) # wp_action_masked
-        spd_ind = np.argmax(spd_action, axis=0)
-        speed_actions = [self.speed_values[i] for i in spd_ind]
+
+        # bool_mask = tf.placeholder(dtype=tf.bool, shape=)
+        # h2 = tf.boolean_mask(h2, bool_mask)
+        # h2 = tf.nn.softmax(h2, name)
+        print('action', action)
+        wp_ind = np.argmax(wp_action, axis=1) # wp_action_masked
+        wp_ind = action % self.wp_action_size
+        spd_ind = np.argmax(spd_action, axis=1)
+        spd_ind = np.floor(action / self.wp_action_size).astype(int)
+        print('spd_ind', spd_ind)
+        speed_actions = [self.speed_values[i[0]] for i in spd_ind]
 
         # Produce action command
         # Get the number of segments in main route index and construct
@@ -451,7 +533,7 @@ class Agent:
             actwp = traf.ap.route[i].iactwp
             if traf.ap.route[i].wptype[actwp] == 3: # actwp == 3 corresponds to destination
                 wp_name = wp_values[1]
-                wp_name[2] = wp_ind[i]
+                wp_name[2] = int(wp_ind[i])
                 self.ac_dict[traf.id[i]] = [0, wp_name]
                 stack.stack("{} ADDWPT RL{}".format(ac_name,  ''.join(map(str, wp_name))))
                 wp_name[1] = wp_name[1] + 1
@@ -461,7 +543,13 @@ class Agent:
 
         # Wp command stack.stack(callsign ADDWPT wpname)
         # Spd command
+    def update_cumreward(self, reward):
+        self.cumreward += reward
 
+    def reset(self):
+        self.write_summaries(self.cumreward)
+        self.cumreward=0
+        self.ac_dict={}
 
 
 
